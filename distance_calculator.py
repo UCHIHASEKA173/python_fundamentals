678a3e2381e18854763b7b6d35cc747999fa85fd
Ryan Seka <122648807+UCHIHASEKA173@users.noreply.github.com>
2023-03-02 08:50:53 -0500

    Add files via upload

---
+++ distance_calculator.py
@@ -0,0 +1,8 @@
+print('-----Distance Calculator-----')
+speed = input('How fast is the vehicle travelling in MPH: ')
+a = int(speed) * 6
+b = int(speed) * 10
+c = int(speed) * 15
+print(f'In six hours the vehicle will travel {a} miles.')
+print(f'In ten hours the vehicle will travel {b} miles.')
+print(f'In fifteen hours the vehicle will travel {c} miles.')
